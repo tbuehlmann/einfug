2d738a0a1461b1d72430fdf10ba1132390df12f1
Tobias Bühlmann <tobias.buehlmann@gmx.de>
2010-09-19 18:09:25 +0200

    added auto highlighting

--- lib/einfug.rb
+++ lib/einfug.rb
@@ -6,13 +6,14 @@ end
 
 class Pathname
   BAD_PATHS = [Pathname.new('.'), Pathname.new('..')]
-  
-  def to_pastie(recursive = false, full = false)
+  EXTENSIONS = {'.rb' => 'ruby', '.java' => 'java'} # TODO
+
+  def to_pastie(recursive = false, full = false, auto_extend = false)
     if self.file?
-      return pastie(self, full)
+      return pastie(self, full, auto_extend)
     elsif self.directory?
       pathnames = []
-      
+
       self.children.each do |pathname|
         if recursive
           pathnames << pathname unless BAD_PATHS.include?(pathname)
@@ -20,26 +21,32 @@ class Pathname
           pathnames << pathname
         end
       end
-      
+
       return pathnames.map do |pathname|
-        pathname.to_pastie(recursive, full)
+        pathname.to_pastie(recursive, full, auto_extend)
       end.compact.join("\n")
     end
   end
-  
+
   private
-  
-  def pastie(pathname, full = false)
+
+  def pastie(pathname, full = false, auto_extend = false)
     if pathname.readable?
       path = full ? pathname.realpath : pathname.basename
-      section = "## "
+
+      extension = if auto_extend && extname = EXTENSIONS[pathname.extname]
+        " [#{extname}]"
+      else
+        ''
+      end
+
       content = begin
         File.read(pathname).sub(/(\r|\n)+\z/, '')
       rescue ArgumentError
         File.read(pathname, :encoding => "BINARY").sub(/(\r|\n)+\z/, '')
       end
-      
-      return "#{section}#{path}\n#{content}"
+
+      return "## #{path}#{extension}\n#{content}"
     end
   end
 end
